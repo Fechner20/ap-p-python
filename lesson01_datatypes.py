f9761161b5172512c13fcd01e3f045bec9979257
Fechner20 <james.d.fechner@gmail.com>
2025-09-24 16:18:01 +0000

    creates datatypes lesson

---
+++ lesson01_datatypes.py
@@ -0,0 +1,32 @@
+#This is a comment
+#DATA TYPES
+
+#1. STRINGS: 
+print(" ")
+print("James>Julian")
+print("once upon a time \nand a very good time it was \nthere was a moocow")
+print("Name:\tJames \nAge: \t14 \nGrade: \t9")
+#INTEGERS
+
+
+print(42)
+print(-23)
+print(41 + 21 + 5)
+
+#FLOAT
+print(4.0)
+print(2.0 + 4.5)
+print( 2.0 + 4)
+
+#BOOLEAN
+print(True) # 1
+print(False) # 0
+print(1==2)
+print(1==1)
+print (1!= 2)
+print("Hello"== "Hello")
+print(6+7==41)
+print(type(4+5))
+print(type("James is Cool"))
+print(type(67.41))
+print(type(1==2))
