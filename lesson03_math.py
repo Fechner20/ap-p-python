af9103e3ecc36fe8e39fe16f3c6ced80a6a12246
Fechner20 <james.d.fechner@gmail.com>
2025-09-26 18:01:42 +0000

    basic math operaters

---
+++ lesson03_math.py
@@ -0,0 +1,39 @@
+#KEY CONCEPTS: math operaters: +, -, *, /, //, %, **
+
+add=6+7
+print("Sum: ", add)
+
+subtract=6-7
+print("Difference: ", subtract)
+
+multiply=6*7
+print("Product: ",multiply)
+
+float_divide=7/6
+print("Quotient:",float_divide)
+
+integer_divide=7//6
+print("Quotient:",integer_divide)
+
+modulus=7%6
+print("Remainder:",modulus)
+
+exponent=6**7
+print("Answer: ", exponent)
+
+print(2+2!=10-6)
+
+result1=(6+7)*13
+print("Result 1:", result1)
+
+result2=2**3*4
+print("Result 2:",result2)
+
+result3=20/5*2
+print("Result 3:",result3)
+
+result4=10-2+3
+print("Result 4:",result4)
+
+result5=5 + 2 ** 3 * (4-1)
+print("Result 5: ", result5)
