8ab9ff8fd9b724d22d1514634f9469d008b65514
Fechner20 <james.d.fechner@gmail.com>
2025-09-30 15:53:48 +0000

    adds math library

---
+++ lesson04_libraries.py
@@ -0,0 +1,10 @@
+#Python libraries are repositories of code you can import into your own code
+
+import math
+print("\n--- Math Library ---\n")
+
+print("Square Root: ", math.sqrt(169))
+print("Round Up: ", math.ceil(6.7))
+print("Round Down:",math.floor(6.7))
+print("Power:" , math.pow(3,3))
+
