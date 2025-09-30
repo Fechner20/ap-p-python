5889cd4a5ee325e702cd5a49033c1992a71abb75
Fechner20 <james.d.fechner@gmail.com>
2025-09-30 16:26:41 +0000

    Random Challenge

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -8,3 +8,18 @@ print("Round Up: ", math.ceil(6.7))
 print("Round Down:",math.floor(6.7))
 print("Power:" , math.pow(3,3))
 
+#------------------
+#  RANDOM LIBRARY
+#------------------
+
+#PRECURSOR CHALLENGE
+
+Seed=5.106
+Step1=Seed*0.7
+Step2=Step1-6.7
+Step3=Seed+4.1
+Step4=math.floor(Step3)
+print(Step4)
+
+ 
+
