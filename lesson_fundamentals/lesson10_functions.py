463a3da777019b63dce8c67a57310cdd5b1ac0f3
James Fechner <james.d.fechner@gmail.com>
2026-02-05 04:00:14 +0000

    challenges

--- lesson_fundamentals/lesson10_functions.py
+++ lesson_fundamentals/lesson10_functions.py
@@ -76,3 +76,107 @@ def double_sequencer(number, times):
 
 result = double_sequencer(6,7)
 print(result)
+
+
+
+#CHALLENGES
+
+
+# Calculator PSEUDO CODE
+#Multiply = (1st number, 2nd number)
+#Answer=(1st number times the 2nd number)
+#Display Answer
+
+#Divide = (1st number, 2nd number)
+#Answer=(1st number divided by the 2nd number)
+#Display Answer
+
+#Add = (1st number, 2nd number)
+#Answer=(1st number plus the 2nd number)
+#Display Answer
+
+#Subtracts = (1st number, 2nd number)
+#Answer=(1st number minus the 2nd number)
+#Display Answer
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+calculator= multiply(5,6)
+print(calculator)
+
+calculator= divide(12,6)
+print(calculator)
+
+calculator= add(5,6)
+print(calculator)
+
+calculator= subtract(10,6)
+print(calculator)
+
+
+# AVERAGE CHALLENGE PSUEDO CODE
+#Average =(1st number, 2nd number, 3rd number)
+#(1st number +2nd number +3rd number)/3
+
+#Display Average
+
+def average(x,y,z):
+    return(x+y+z)/3
+
+av=average(2,10,17)
+print(av)
+
+
+#EVEN OR ODD CHALLENGE
+#IS EVEN= Number
+#Remain=Number modulus 2
+# If remain is equal to 0:
+#   the number is even
+#Otherwise its odd
+
+def eo(x):
+    remain= x % 2
+    if remain == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+
+eoo = eo(8)
+print(eoo)
+
+
+#ANYLYZE WORD CHALLENGE
+
+def anylyzeword(x):
+    word = x
+    vowelcount=0
+    consonantcount=0
+    for vowel in word:
+        if vowel == "a" or vowel == "e" or vowel== "i" or vowel==  "o" or vowel==  "u" or vowel == "y":
+            vowelcount = vowelcount + 1
+        else:
+            consonantcount= consonantcount + 1
+    return f" In the word {word} there are {vowelcount} vowels and {consonantcount} consonants."
+    
+
+
+wordstats=anylyzeword("James")
+print(wordstats)
+
+
+
+
+
+
