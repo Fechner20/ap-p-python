00d7d7cc326537350214b8a0c6dddd93bfc55079
James Fechner <james.d.fechner@gmail.com>
2026-03-27 16:27:34 +0000

    x

---
+++ flow_challenges.py
@@ -0,0 +1,16 @@
+# num = 4
+# while num < 2:
+#     num += 2
+# print(num)
+# while num < 8:
+#     num += 1
+# print(num)
+
+#Challenge 2
+
+num = 7
+
+while num != 5 and num % 2 != 0:
+    num +=1
+print(num)
+    
---
+++ flow_swap02.py
@@ -0,0 +1,7 @@
+def allequal(x,y,z):
+    if x == y == z:
+        print(True)
+    else:
+        print(False)
+    
+allequal(10,20,10)
